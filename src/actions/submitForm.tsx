200b199a1966b7ceae937c6fa078978402545554
Muhammad Zunnoorain Rafi <bhoshyl@gmail.com>
2023-11-15 22:49:20 +0500

    added task successfully

--- src/actions/submitForm.tsx
+++ src/actions/submitForm.tsx
@@ -1,5 +1,9 @@
 'use server';
 
+import { prismaDB } from '@/utils/db';
+import { revalidatePath } from 'next/cache';
+import { z } from 'zod';
+
 type State = {
   errors?: {
     title?: string[];
@@ -8,4 +12,31 @@ type State = {
   message?: string | null;
 };
 
-export const submitForm = (prevState: State, formData: FormData) => {};
+const formSchema = z.object({
+  title: z.string().min(1, 'Enter title'),
+  description: z.string().min(1, 'Enter description'),
+});
+
+export async function submitForm(prevState: State, formData: FormData) {
+  const validation = formSchema.safeParse({
+    title: formData.get('title'),
+    description: formData.get('description'),
+  });
+  if (!validation.success) {
+    return {
+      errors: validation.error.flatten().fieldErrors,
+      message: 'Something went wrong',
+    };
+  }
+  try {
+    await prismaDB.task.create({
+      data: {
+        title: validation.data.title,
+        description: validation.data.description,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+  revalidatePath('/');
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,37 @@
+import { prismaDB } from '@/utils/db';
 import Form from './form';
 
-export default function Home() {
+export default async function Home() {
+  const allTasks = await prismaDB.task.findMany({
+    orderBy: {
+      createdAt: 'desc',
+    },
+  });
+  const dateC = (date: Date) => {
+    return new Date(date).toLocaleDateString();
+  };
+
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <main>
-        <div className="max-w-4xl">
+      <main className=" w-96 ">
+        <div className="mb-3">
           <Form />
+        </div>
+        <div className="space-y-2">
+          {allTasks.map((task) => (
+            <div
+              className="p-2 rounded-md border border-slate-600 flex items-center justify-between"
+              key={task.id}
+            >
+              <div>
+                <p className="font-bold text-slate-50 ">{task.title}</p>
+                <p className="text-slate-200">{task.description}</p>
+              </div>
+              <p className="text-sm italic text-slate-400">
+                {dateC(task.createdAt)}
+              </p>
+            </div>
+          ))}
         </div>
       </main>
     </div>
